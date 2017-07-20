33a2631beb194173766f9455a4683cdb22f9eb7e
lishidetainan <1174487899@qq.com>
2017-07-20 17:11:31 +0800

    分支提交

--- src/test/java/com/tanghd/spring/dbutil/test/TestClass.java
+++ src/test/java/com/tanghd/spring/dbutil/test/TestClass.java
@@ -4,6 +4,7 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		System.out.println("this is test");
-		System.out.println("this is wgc test commit");
+		
+		System.out.println("8989899");
 	}
 }
