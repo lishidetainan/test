fd225a24e9ea8b1696e7a535ad392a69b7975619
lishidetainan <1174487899@qq.com>
2017-07-24 14:20:56 +0800

    ce

--- src/test/java/com/tanghd/spring/dbutil/test/TestClass.java
+++ src/test/java/com/tanghd/spring/dbutil/test/TestClass.java
@@ -3,11 +3,6 @@ package com.tanghd.spring.dbutil.test;
 public class TestClass {
 
 	public static void main(String[] args) {
-		System.out.println("this is test");
-		
-		System.out.println("8989899");
-		System.out.println("this is master test commit");
-		
-		System.out.println("this is wgc test commit");
+		System.out.println("this is wgc breach test");
 	}
 }
