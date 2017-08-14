de4ea45da1073c815e6e0cb672898c60ae98e0d8
lishidetainan <1174487899@qq.com>
2017-08-14 17:36:55 +0800

    wgc11111

--- src/test/java/com/tanghd/spring/dbutil/test/TestClass.java
+++ src/test/java/com/tanghd/spring/dbutil/test/TestClass.java
@@ -3,6 +3,6 @@ package com.tanghd.spring.dbutil.test;
 public class TestClass {
 
 	public static void main(String[] args) {
-		System.out.println("this is my master_test test");
+		System.out.println("this is my wgc_test test");
 	}
 }
