0dc38bc2319f937f6d2c059ff4aa223e875011dc
lishidetainan <1174487899@qq.com>
2017-08-14 16:59:15 +0800

    测试

--- src/test/java/com/tanghd/spring/dbutil/test/TestClass.java
+++ src/test/java/com/tanghd/spring/dbutil/test/TestClass.java
@@ -3,8 +3,6 @@ package com.tanghd.spring.dbutil.test;
 public class TestClass {
 
 	public static void main(String[] args) {
-		System.out.println("this is wgc breach test");
-		System.out.println("welcome to china");
-		System.out.println("this is");
+		
 	}
 }
